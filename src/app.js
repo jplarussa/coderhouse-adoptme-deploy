1a1d5c7dfaddbfce271debffd3181daecf863582
Jean Pierre La Russa <jplarussa@gmail.com>
2023-06-29 21:26:01 -0300

    Add feature views

--- src/app.js
+++ src/app.js
@@ -1,15 +1,24 @@
 import express from 'express';
 import mongoose from 'mongoose';
 import cookieParser from 'cookie-parser';
-
+import handlebars from 'express-handlebars';
+import __dirname from './util.js';
 import usersRouter from './routes/users.router.js';
 import petsRouter from './routes/pets.router.js';
 import adoptionsRouter from './routes/adoption.router.js';
 import sessionsRouter from './routes/sessions.router.js';
+import viewsRouter from './routes/views.router.js';
+
 
 const app = express();
 const PORT = process.env.PORT||9090;
 const connection = mongoose.connect(process.env.MONGO_URL)
+
+
+// Define the template engine and views
+app.engine('handlebars', handlebars.engine());
+app.set('view engine', 'handlebars');
+app.set('views', __dirname + "/views");
 
 app.use(express.json());
 app.use(cookieParser());
@@ -18,5 +27,6 @@ app.use('/api/users',usersRouter);
 app.use('/api/pets',petsRouter);
 app.use('/api/adoptions',adoptionsRouter);
 app.use('/api/sessions',sessionsRouter);
+app.use("/", viewsRouter);
 
 app.listen(PORT,()=>console.log(`Listening on ${PORT}`))
